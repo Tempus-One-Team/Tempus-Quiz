cdac8c2a9c54d614ba15cc131d6434f47cc558a8
AipovAlexander <qweqweqwe1465@gmail.com>
2024-01-03 14:19:13 +0400

    Add route

---
+++ src/router/index.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { AppRoutesPath } from "./types";
+import CurrentProgrammerCodePage from "../pages/current-programmer-code-page";
+import EnterRoom from "../pages/enter-room";
+import InvitationPage from "../pages/invitation-page";
+import ListOfAllProgrammers from "../pages/list-of-all-programmers";
+import TaskDescription from "../pages/task-description";
+import TaskPageWithRules from "../pages/task-page-with-rules";
+import WaitingRoom from "../pages/waiting-room";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route element={} />
+    </Routes>
+  );
+};
