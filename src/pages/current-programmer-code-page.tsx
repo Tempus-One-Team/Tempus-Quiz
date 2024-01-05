a83bc9c4bd833ba81b71c2c3114e54dbe9be4b0e
AipovAlexander <qweqweqwe1465@gmail.com>
2024-01-05 08:09:49 +0400

    Add current prommager code page

--- src/pages/current-programmer-code-page.tsx
+++ src/pages/current-programmer-code-page.tsx
@@ -1,10 +1,36 @@
-import Paragraph from 'antd/es/typography/Paragraph';
+import { Space, Typography } from 'antd';
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live';
+
+const { Title } = Typography;
 
 const CurrentProgrammerCodePage = () => {
     return (
-        <>
-            <Paragraph>Current Programmer Code Page</Paragraph>
-        </>
+        <Space direction="horizontal">
+            <Space direction="vertical">
+                <Title>Задача 1</Title>
+                <Title>Описание задачи</Title>
+                <Title>Что принимает задача?</Title>
+                <Title>Что возвращает задача?</Title>
+            </Space>
+            <Space>
+                <Title> Код</Title>
+                <LiveProvider code="const example = 'Hello World';">
+                    <LiveEditor
+                        style={{
+                            tabSize: 5,
+                            padding: '50px',
+                            width: '400px !important',
+                            height: '600px',
+                            border: '1px solid black',
+                            margin: '50px',
+                        }}
+                    />
+                    <LiveError />
+                    <Title level={3}>Результат:</Title>
+                    <LivePreview />
+                </LiveProvider>
+            </Space>
+        </Space>
     );
 };
 
