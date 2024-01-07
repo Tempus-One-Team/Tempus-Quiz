473822ba5afd56fa30ef03d0fcc2fc8f927bb6c8
Efimov <nikitacanai@yandex.ru>
2024-01-07 17:18:22 +0700

    login fake user

--- src/pages/about-room.tsx
+++ src/pages/about-room.tsx
@@ -1,23 +1,28 @@
+import { Space } from 'antd';
 import Paragraph from 'antd/es/typography/Paragraph';
 import { sendInviteToLobby } from 'api/lobbyes/send-invite-to-lobby';
 import Cookies from 'js-cookie';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import { AppRoutesPath } from 'router/types';
 import { setButtons } from 'store/reducers/footer-slice';
+import { UserType, setUser } from 'store/reducers/user-slice';
 import { useAppDispatch, useAppSelector } from 'store/store-hooks';
+import checkForEmptyValues from 'utils/valudate/checkForEmpty';
 
 const AboutRoom = () => {
     const { id } = useParams();
     const dispatch = useAppDispatch();
     const UserId = Cookies.get('userId');
     const User = useAppSelector((state) => state.user);
+    const [ActiveUser, setActiveUser] = useState<UserType>(User);
     const navigate = useNavigate();
 
     // отобразить описание комнаты, участников, приз, и с права форму с полями имя почта и фото
     // попробуй сам сделать запрос на сервер, как пример возьми get-lobby-from-code
     // если комнаты по такому id не будет то просто выводи что такой комнаты нет
-    // кнопку я добавил сам 
+    // если что то не поймешь то пиши мне
 
     useEffect(() => {
         dispatch(
@@ -26,20 +31,57 @@ const AboutRoom = () => {
                     label: 'Присоедениться',
                     onClick: () => {
                         if (id) {
-                            sendInviteToLobby(id, User, 'LOLID'); // тут вообще будет другой айди но так как щас я на акке админа пусть будет так
-                            Cookies.set('selectLobby', id);
-                            navigate(AppRoutesPath.WAITING_ROOM);
+                            if (User.isLogin) {
+                                // здесь еще нужна проверка на админа, если этот пользователь админ этой комнаты то редирект на invitation-page
+                                sendInviteToLobby(id, User, UserId); // здесь поменять потом на id который будет если команда найдена
+                                Cookies.set('selectLobby', id);
+                                navigate(AppRoutesPath.WAITING_ROOM);
+                            } else {
+                                if (checkForEmptyValues(ActiveUser)) {
+                                    sendInviteToLobby(id, ActiveUser); // здесь поменять потом на id который будет если команда найдена
+                                    Cookies.set('selectLobby', id);
+                                    navigate(AppRoutesPath.WAITING_ROOM);
+                                }
+                            }
                         }
                     },
-
-                    // здесь поменять потом на id который будет если команда найдена
-                    // а User и UserId нужно взять из полей которые ты напишешь с правой стороны, userID можно сделать что бы само находилось значение уникальное, либо напиши мне и я сделаю чтоб это сделал сам сервер
                 },
             ]),
         );
-    }, []); // в зависимости добавь инпуты формы с права
+    }, [ActiveUser]);
 
-    return <Paragraph>About Room {id}</Paragraph>;
+    return (
+        <Paragraph>
+            About Room {id}
+            {!User.isLogin && (
+                <Space>
+                    <input
+                        type="text"
+                        onChange={(e) =>
+                            setActiveUser((prev) => ({ ...prev, UserName: e.target.value }))
+                        }
+                        placeholder="Name"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        onChange={(e) =>
+                            setActiveUser((prev) => ({ ...prev, UserEmail: e.target.value }))
+                        }
+                    />
+                    <input
+                        type="text"
+                        placeholder="photo"
+                        onChange={
+                            (e) => setActiveUser((prev) => ({ ...prev, UserPhoto: e.target.value })) // здесь используй hendler-image-upload для картинки
+                        }
+                    />
+                    или войди с Tempus ID
+                    <Link to={AppRoutesPath.SIGN_IN_PAGE}>ссылка</Link>
+                </Space>
+            )}
+        </Paragraph>
+    );
 };
 
 export default AboutRoom;
--- src/utils/valudate/checkForEmpty.tsx
+++ src/utils/valudate/checkForEmpty.tsx
@@ -1,6 +1,7 @@
+import { UserType } from 'store/reducers/user-slice';
 import { LobbyInfo, LobbyTask } from 'types/lobbyTypes';
 
-export default function checkForEmptyValues(obj: LobbyInfo | LobbyTask) {
+export default function checkForEmptyValues(obj: LobbyInfo | LobbyTask | UserType) {
     for (const key in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
             if (obj[key] === '' || obj[key] === null || obj[key] === undefined) {
