ef78f1995e8b6f7f1f2264bcb42435ef52bfd695
Efimov <93506814+YAcanai@users.noreply.github.com>
2024-01-09 12:26:01 +0300

    fix ts

--- src/utils/crypt-data/cripting-data.ts
+++ src/utils/crypt-data/cripting-data.ts
@@ -2,11 +2,11 @@ import CryptoJS from 'crypto-js';
 
 const myKey = 'T3mPUsK3y@$$';
 
-export function encryptData(data) {
+export function encryptData(data: string) {
     return CryptoJS.AES.encrypt(data, myKey).toString();
 }
 
-export function decryptData(encryptedData) {
+export function decryptData(encryptedData: string) {
     const bytes = CryptoJS.AES.decrypt(encryptedData, myKey);
     return bytes.toString(CryptoJS.enc.Utf8);
 }
